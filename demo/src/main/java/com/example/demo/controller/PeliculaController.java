495012451a57388479368a652a29b5655b81b3c8
Giovanny Otarola <giovannyxdd@gmail.com>
2024-04-19 17:31:11 -0400

    Update PeliculaController.java

--- demo/src/main/java/com/example/demo/controller/PeliculaController.java
+++ demo/src/main/java/com/example/demo/controller/PeliculaController.java
@@ -2,6 +2,9 @@ package com.example.demo.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,23 +17,44 @@ import com.example.demo.service.PeliculaService;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.swing.text.html.parser.Entity;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
-
-
+import org.hibernate.mapping.Collection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 @RestController
 @RequestMapping("/peliculas")
 public class PeliculaController {
+
+    private static final Logger log = LoggerFactory.getLogger(PeliculaController.class);
+
     @Autowired
     private PeliculaService peliculaService;
 
     @GetMapping
-    public List<Pelicula> getAllPeliculas() {
-    return peliculaService.getAllPeliculas();
+    public CollectionModel<EntityModel<Pelicula>> getAllPeliculas() {
+        List<Pelicula> peliculas = peliculaService.getAllPeliculas();
+        log.info("GET /peliculas");
+        log.info("Retornando todas las Peliculas");
+        List<EntityModel<Pelicula>> peliculasResource = peliculas.stream()
+            .map( pelicula -> EntityModel.of(pelicula,
+            WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getPeliculaById(pelicula.getId())).withSelfRel()
+        ))
+            .collect(Collectors.toList());
+
+        WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).getAllPeliculas());
+        CollectionModel<EntityModel<Pelicula>> resources = CollectionModel.of(peliculasResource, linkTo.withRel("peliculas"));
+
+        return resources;
+
     }
 
 
